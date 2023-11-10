b92f8e09f85ec5b193907f05289a67b9df515971
yubin <dbqls2821@gmail.com>
2023-11-11 01:19:38 +0900

    feat: 출력을 담당하는 추상 인터페이스 추가

---
+++ src/main/java/christmas/io/OutputPort.java
@@ -0,0 +1,28 @@
+package christmas.io;
+
+import java.util.List;
+
+public interface OutputPort {
+    void printIntroduction();
+
+    void printInputDate();
+
+    void printInputMenu();
+
+    void printBenefitPreview();
+
+    void printOrderedMenu(List<String> menus);
+
+    void printPriceBeforeBenefit(int price);
+
+    void printGiveawayMenu(String menu);
+
+    void printBenefitList(List<String> list);
+
+    void printBenefitPrice(int price);
+
+    void printPriceAfterBenefit(int price);
+
+    void printEventBadge(String badge);
+
+}
