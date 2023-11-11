b7cfcbf1fbfa2454a12893d29a038e2ecfdfbdb6
yubin <dbqls2821@gmail.com>
2023-11-12 00:21:27 +0900

    feat: Course요리에 Meal List 추가

--- src/main/java/christmas/domain/Course.java
+++ src/main/java/christmas/domain/Course.java
@@ -1,8 +1,17 @@
 package christmas.domain;
 
+import java.util.List;
+
 enum Course {
-    APPETIZER,
-    MAIN,
-    DESSERT,
-    BEVERAGE
+    APPETIZER(List.of(Meal.MUSHROOM_SOUP, Meal.TAPAS, Meal.CAESAR_SALAD)),
+    MAIN(List.of(Meal.T_BONE_STEAK, Meal.BBQ_RIBS, Meal.SEAFOOD_PASTA, Meal.CHRISTMAS_PASTA)),
+    DESSERT(List.of(Meal.CHOCOLATE_CAKE, Meal.ICE_CREAM)),
+    BEVERAGE(List.of(Meal.ZERO_COLA, Meal.RED_WINE, Meal.CHAMPAGNE)),
+    ;
+
+    private final List<Meal> meals;
+
+    Course(List<Meal> meals) {
+        this.meals = meals;
+    }
 }
