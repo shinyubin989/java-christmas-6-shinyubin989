d500f3e17b24666baa8963c0c6b08a3d8a337b03
yubin <dbqls2821@gmail.com>
2023-11-12 00:06:29 +0900

    feat: 유효하지 않은 날짜에 대한 예외 메시지 상수 추가

--- src/main/java/christmas/domain/DomainException.java
+++ src/main/java/christmas/domain/DomainException.java
@@ -6,6 +6,7 @@ enum DomainException {
     MENU_NUM_MUST_BE_AT_LEAST_ONE(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
     MENU_FORMAT_IS_NOT_VALID(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
     MENU_IS_DUPLICATED(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_DATE(ERROR.message + "유효하지 않은 날짜입니다. 다시 입력해 주세요."),
 
     ;
     private final String message;
