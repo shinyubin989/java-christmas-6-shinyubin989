6ab5ea0a808356eeeba92a9488c756b16c6295e9
yubin <dbqls2821@gmail.com>
2023-11-12 00:08:09 +0900

    feat: 예외가 나지 않을 때까지 날짜를 입력받는 Controller 메서드추가

---
+++ src/main/java/christmas/domain/OrderController.java
@@ -0,0 +1,32 @@
+package christmas.domain;
+
+import christmas.dto.OrderDto;
+import christmas.io.InputPort;
+import christmas.io.OutputPort;
+
+import java.time.DateTimeException;
+import java.util.List;
+
+public final class OrderController {
+
+    private final InputPort inputPort;
+    private final OutputPort outputPort;
+
+    OrderController(InputPort inputPort, OutputPort outputPort) {
+        this.inputPort = inputPort;
+        this.outputPort = outputPort;
+    }
+
+    VisitDate receiveDate() {
+        while (true) {
+            try {
+                return new VisitDate(inputPort.readDate());
+            } catch (IllegalArgumentException e) {
+                outputPort.printException(e.getMessage());
+            } catch (DateTimeException e) {
+                outputPort.printException(DomainException.INVALID_DATE.getMessage());
+            }
+        }
+    }
+
+}
