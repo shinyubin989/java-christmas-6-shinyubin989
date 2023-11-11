b2f8b78dd8d6f4b3b738435d0e5188c8bd111530
yubin <dbqls2821@gmail.com>
2023-11-11 23:17:33 +0900

    feat: 입력 예외 메시지 추가

--- src/main/java/christmas/io/IoException.java
+++ src/main/java/christmas/io/IoException.java
@@ -4,7 +4,8 @@ public enum IoException {
     ERROR("[ERROR] "),
     DATE_IS_NOT_NUMBER(ERROR.message + "날짜 입력값이 숫자가 아닙니다. 다시 입력해 주세요."),
     INVALID_ORDER(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    NUM_OF_ORDER_IS_NOT_NUMBER(ERROR.message + "주문하신 갯수는 숫자가 아닙니다. 다시 입력해 주세요."),
+    NUM_OF_ORDER_IS_NOT_NUMBER(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_FORMAT_IS_INVALID(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ;
 
     private final String message;
