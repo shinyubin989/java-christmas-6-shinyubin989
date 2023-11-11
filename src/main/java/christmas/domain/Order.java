dd17a610782ae7339f8fb768f37c203c44ca22c5
yubin <dbqls2821@gmail.com>
2023-11-11 22:27:37 +0900

    feat: 메뉴에 중복이 있을시 예외를 던지는 로직 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,17 +1,37 @@
 package christmas.domain;
 
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
-record Order(List<OrderUnit> list){
+class Order {
 
-    Order{
+    private final List<OrderUnit> order = new ArrayList<>();
 
+    public Order(List<OrderUnit> order) {
+        validateDuplication(order);
     }
 
-    String toFormat() {
+    private void validateDuplication(List<OrderUnit> order) {
+        if(order.size() != order.stream().distinct().count())
+            throw new IllegalArgumentException(DomainException.MENU_IS_DUPLICATED.getMessage());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order target = (Order) o;
+        return Objects.equals(order, target.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order);
+    }
+
+    @Override
+    public String toString() {
         StringJoiner joiner = new StringJoiner("\n");
-        list.forEach(it -> joiner.add(it.toString()));
+        order.forEach(it -> joiner.add(it.toString()));
         return joiner.toString();
     }
 }
