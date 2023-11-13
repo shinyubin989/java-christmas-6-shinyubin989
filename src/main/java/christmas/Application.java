7bf701a675b09131e2b093cb0e4737c8fd6433e3
yubin <dbqls2821@gmail.com>
2023-11-14 01:27:14 +0900

    feat: 전반적인 main로직 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,19 @@
 package christmas;
 
+import christmas.domain.*;
+import christmas.io.ConsoleInputReader;
+import christmas.io.InputView;
+import christmas.io.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OrderController controller = new OrderController(new InputView(new ConsoleInputReader()), new OutputView());
+        PosMachine posMachine = new PosMachine();
+
+        VisitDate date = controller.receiveDate();
+        Order order = controller.receiveOrder();
+        Receipt receipt = posMachine.printReceipt(date, order);
+
+        controller.showReceipt(date, receipt);
     }
 }
