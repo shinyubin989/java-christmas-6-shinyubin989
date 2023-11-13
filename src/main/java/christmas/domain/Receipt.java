f2b104c9b1e2c33e44ec0a4445f9bd376cca4701
yubin <dbqls2821@gmail.com>
2023-11-13 23:06:43 +0900

    fix: receipt내에 order 제거

--- src/main/java/christmas/domain/Receipt.java
+++ src/main/java/christmas/domain/Receipt.java
@@ -4,8 +4,7 @@ import christmas.domain.benefit.Benefits;
 
 import java.util.Map;
 
-record Receipt(Order orderMenus, int priceBeforeBenefit,
-               Giveaway giveaway, Map<Benefits, Integer> benefits,
+record Receipt(int priceBeforeBenefit, Giveaway giveaway, Map<Benefits, Integer> benefits,
                int priceAfterBenefit, String badge
 ) {
 }
