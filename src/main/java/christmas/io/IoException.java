82327fa087fc6bbee21d7eb1535b09acda62033e
yubin <dbqls2821@gmail.com>
2023-11-14 00:44:50 +0900

    fix: 날짜 입력값 예외문구 수정

--- src/main/java/christmas/io/IoException.java
+++ src/main/java/christmas/io/IoException.java
@@ -2,7 +2,7 @@ package christmas.io;
 
 public enum IoException {
     ERROR("[ERROR] "),
-    DATE_IS_NOT_NUMBER(ERROR.message + "날짜 입력값이 숫자가 아닙니다. 다시 입력해 주세요."),
+    DATE_IS_NOT_NUMBER(ERROR.message + "유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_ORDER(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
     NUM_OF_ORDER_IS_NOT_NUMBER(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
     MENU_FORMAT_IS_INVALID(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요."),
