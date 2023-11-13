483e090ece49d0aa770cb681d6da12f1923b4190
yubin <dbqls2821@gmail.com>
2023-11-13 19:01:12 +0900

    feat: 크리스마스마스까지 남은 일수 계산 메소드 구현

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -20,6 +20,10 @@ class VisitDate {
         return SpecificBenefitDays.isSpecificBenefitDays(date.getDayOfMonth());
     }
 
+    int daysLeftUntilChristmas() {
+        return Constraint.CHRISTMAS_DATE.getValue() - date.getDayOfMonth();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
