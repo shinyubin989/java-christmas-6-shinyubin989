ca580d12316eb720be80e3f42223d5e8967cd395
yubin <dbqls2821@gmail.com>
2023-11-14 01:34:04 +0900

    test: 날짜 입력값이 숫자가 아닐경우 예외를 발생시키는 테스트코드 추가

--- src/test/java/christmas/io/InputViewTest.java
+++ src/test/java/christmas/io/InputViewTest.java
@@ -2,6 +2,7 @@ package christmas.io;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
@@ -11,6 +12,14 @@ class InputViewTest {
         InputView inputView = new InputView(() -> "3");
         int date = inputView.readDate();
         assertEquals(3, date);
+    }
+
+    @Test
+    void 날짜_입력값이_숫자가_아닐경우_예외를_발생시킨다() {
+        InputView inputView = new InputView(() -> "a");
+        assertThatThrownBy(inputView::readDate)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
 }
