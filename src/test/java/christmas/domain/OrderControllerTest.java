a39273ce5702c7564e4a6bc37b05c737fdcc4ddf
yubin <dbqls2821@gmail.com>
2023-11-14 18:24:22 +0900

    test: 주문메뉴 입력값을 받아와서 객체로 반환하는 테스트코드 추가

--- src/test/java/christmas/domain/OrderControllerTest.java
+++ src/test/java/christmas/domain/OrderControllerTest.java
@@ -1,8 +1,11 @@
 package christmas.domain;
 
+import christmas.dto.OrderDto;
 import christmas.io.InputView;
 import christmas.io.OutputView;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +16,15 @@ class OrderControllerTest {
         OrderController orderController = new OrderController(new InputView(() -> "3"), new OutputView());
         VisitDate date = orderController.receiveDate();
         assertEquals(new VisitDate(3), date);
+    }
+
+    @Test
+    void 주문메뉴_입력값을_받아와서_객체로_반환한다() {
+        OrderController orderController = new OrderController(new InputView(() -> "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"), new OutputView());
+
+        Order expected = new Order(List.of(new OrderDto("티본스테이크", 1), new OrderDto("바비큐립", 1), new OrderDto("초코케이크", 2), new OrderDto("제로콜라", 1)));
+
+        assertEquals(expected, orderController.receiveOrder());
     }
 
 }
