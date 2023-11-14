7eb3cb722878bf79b77a13d852024f7a6192e4d9
yubin <dbqls2821@gmail.com>
2023-11-14 17:21:36 +0900

    test: 해당하는 메뉴 이름이 없는 경우 예외를 반환하는 테스트코드 추가

--- src/test/java/christmas/domain/MealTest.java
+++ src/test/java/christmas/domain/MealTest.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MealTest {
@@ -13,5 +14,12 @@ class MealTest {
         Meal actual = Meal.findByName(mealName);
 
         assertEquals(Meal.MUSHROOM_SOUP, actual);
+    }
+
+    @Test
+    void 해당하는_메뉴_이름이_없는경우_예외를_반환한다() {
+        assertThatThrownBy(() -> Meal.findByName("없는이름"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
