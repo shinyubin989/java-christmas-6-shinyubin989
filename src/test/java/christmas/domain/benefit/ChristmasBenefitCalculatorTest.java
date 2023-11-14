8dfe80f5232a3da7294c4289b74bd6b2901acdf9
yubin <dbqls2821@gmail.com>
2023-11-14 16:12:24 +0900

    test: 크리스마스 할인 혜택 적용 날짜인 경우 할인 금액을 반환하는 테스트코드 추가

---
+++ src/test/java/christmas/domain/benefit/ChristmasBenefitCalculatorTest.java
@@ -0,0 +1,24 @@
+package christmas.domain.benefit;
+
+import christmas.domain.Order;
+import christmas.domain.VisitDate;
+import christmas.dto.OrderDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChristmasBenefitCalculatorTest {
+
+    @Test
+    void 크리스마스_할인_혜택_적용_날짜인경우_할인_금액을_반환한다() {
+        Order order = new Order(List.of(new OrderDto("양송이수프", 2)));
+        VisitDate visitDate = new VisitDate(25);
+
+        int actual = new ChristmasBenefitCalculator().calculate(order, visitDate);
+
+        assertEquals(3400, actual);
+    }
+
+}
