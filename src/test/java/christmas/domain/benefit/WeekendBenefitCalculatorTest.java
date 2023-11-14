80b16f3262ef8ebd965acfed5be7565b658dc144
yubin <dbqls2821@gmail.com>
2023-11-14 16:52:33 +0900

    test: 주말인 경우의 할인 금액을 반환하는 테스트코드 추가

---
+++ src/test/java/christmas/domain/benefit/WeekendBenefitCalculatorTest.java
@@ -0,0 +1,24 @@
+package christmas.domain.benefit;
+
+import christmas.domain.Order;
+import christmas.domain.VisitDate;
+import christmas.dto.OrderDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeekendBenefitCalculatorTest {
+
+    @Test
+    void 주말인경우_메인메뉴의_할인_금액을_반환한다() {
+        Order order = new Order(List.of(new OrderDto("크리스마스파스타", 2), new OrderDto("아이스크림", 2)));
+        VisitDate visitDate = new VisitDate(29);
+
+        int actual = new WeekendBenefitCalculator().calculate(order, visitDate);
+
+        assertEquals(4046, actual);
+    }
+
+}
