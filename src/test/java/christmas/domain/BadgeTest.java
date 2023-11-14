83cab55dc74a5b14f9493ef98b925106fde3d908
yubin <dbqls2821@gmail.com>
2023-11-14 16:57:34 +0900

    test: 혜택 금액이 만원 이상일 경우 트리 배지를 반환하는 테스트코드 추가

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -14,10 +14,17 @@ class BadgeTest {
     }
 
     @Test
-    void 혜택금액이_오천원_이상일_경우_배지를_반환하지_않는다() {
+    void 혜택금액이_오천원_이상일_경우_스타_배지를_반환한다() {
         String actual = Badge.findBadgeByBenefitPrice(9999);
 
         assertEquals(Badge.STAR.getBadgeName(), actual);
+    }
+
+    @Test
+    void 혜택금액이_만원_이상일_경우_트리_배지를_반환한다() {
+        String actual = Badge.findBadgeByBenefitPrice(19999);
+
+        assertEquals(Badge.TREE.getBadgeName(), actual);
     }
 
 }
