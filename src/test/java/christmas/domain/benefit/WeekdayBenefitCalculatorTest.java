48013464a9b38326edc4c733067446f9a85b70f2
yubin <dbqls2821@gmail.com>
2023-11-14 16:51:14 +0900

    test: 평일이 아닌 경우의 할인 금액을 반환하는 테스트코드 추가

--- src/test/java/christmas/domain/benefit/WeekdayBenefitCalculatorTest.java
+++ src/test/java/christmas/domain/benefit/WeekdayBenefitCalculatorTest.java
@@ -21,4 +21,14 @@ class WeekdayBenefitCalculatorTest {
         assertEquals(4046, actual);
     }
 
+    @Test
+    void 평일이_아닌경우_할인_금액을_반환한다() {
+        Order order = new Order(List.of(new OrderDto("양송이수프", 2), new OrderDto("아이스크림", 2)));
+        VisitDate visitDate = new VisitDate(29);
+
+        int actual = new WeekdayBenefitCalculator().calculate(order, visitDate);
+
+        assertEquals(0, actual);
+    }
+
 }
