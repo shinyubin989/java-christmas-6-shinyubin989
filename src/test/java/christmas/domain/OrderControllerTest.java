8ea54604cf849520142489a5edade8c31b1a8305
yubin <dbqls2821@gmail.com>
2023-11-14 18:28:07 +0900

    test: 영수증 출력 과정에서 예외가 발생하지 않는 테스트코드 추가

--- src/test/java/christmas/domain/OrderControllerTest.java
+++ src/test/java/christmas/domain/OrderControllerTest.java
@@ -27,4 +27,15 @@ class OrderControllerTest {
         assertEquals(expected, orderController.receiveOrder());
     }
 
+    @Test
+    void 영수증_출력_과정에서_예외가_발생하지_않는다() {
+        PosMachine posMachine = new PosMachine();
+        OrderController orderController = new OrderController(new InputView(() -> "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"), new OutputView());
+        VisitDate visitDate = new VisitDate(3);
+        Order order = new Order(List.of(new OrderDto("티본스테이크", 1), new OrderDto("바비큐립", 1), new OrderDto("초코케이크", 2), new OrderDto("제로콜라", 1)));
+        Receipt receipt = posMachine.printReceipt(visitDate, order);
+
+        assertDoesNotThrow(() -> orderController.showReceipt(visitDate, receipt));
+    }
+
 }
